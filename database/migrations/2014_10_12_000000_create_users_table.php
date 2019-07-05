dee8cd66a6bf2b040df27ee16804a62eba704f07
Salvo La Torre <latorresalvo@gmail.com>
2019-07-05 20:38:25 +0200

    create migration tabella Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('nome');
+            $table->string('cognome');
+            $table->string('nickname')->unique();
+            $table->date('dataNascita');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
