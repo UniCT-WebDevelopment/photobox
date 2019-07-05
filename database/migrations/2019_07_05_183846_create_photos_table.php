d3f7e7990c817516bdc1fac590041b1df116db71
Salvo La Torre <latorresalvo@gmail.com>
2019-07-05 20:45:03 +0200

    create photos table migration

---
+++ database/migrations/2019_07_05_183846_create_photos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('descrizione');
+            $table->string('GPS');
+            $table->date('dataCaricamento');
+            $table->bigInteger('idUtente');
+            $table->timestamps();
+
+            $table->foreign('idUtente')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
