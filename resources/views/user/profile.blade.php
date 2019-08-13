7ee5671f59fd8bff13c9d8146dd08f155dd8c40f
Salvatore La Torre <salvatorelatorre@MBPdiSalvatore.lan>
2019-08-13 17:21:33 +0200

    Aggiunto campo bio e fix date format

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -5,7 +5,8 @@
     <h6><strong>Nome</strong>: {{ $user->nome }}</h6>
     <h6><strong>Cognome</strong>: {{ $user->cognome }}</h6>
     <h6><strong>Username</strong>: {{ $user->nickname }}</h6>
-    <h6><strong>Data di Nascita</strong>: {{ $user->dataNascita }}</h6>
+    <h6><strong>Data di Nascita</strong>: {{ date('d-m-Y', strtotime($user->dataNascita)) }}</h6>
+    <h6><strong>Biografia</strong>: {{ $user->bio }}</h6>
    
     <div class="section_content mt30">
         <a href="/modify" class="btn btn-primary-outline">Modifica Profilo</a>
