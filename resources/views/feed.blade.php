41ae72423880f019908bb23980f8c95d99e9e481
Salvatore La Torre <latorresalvo@gmail.com>
2019-08-28 18:47:27 +0200

    fix responsive feed page

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -39,18 +39,18 @@
         <h1>Bacheca</h1>
         <div class="row">
             <!-- Photo Start-->
-            <div class="col-md-3">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4">
                 <div class="photo-container">
                     <div class="row photo-user">
-                        <div class="col-md-8">
+                        <div class="col-md-7">
                             {{ $user->nickname }}
                         </div>
-                        <div class="col-md-4 text-right">
+                        <div class="col-md-5 text-right">
                             28/08/2019
                         </div>
                     </div>
                     <div class="photo-border"></div>
-                    <img src="images/cocoon/portfolio/home-port4.png" alt="cocoon">
+                    <img class="img-fluid" src="images/cocoon/portfolio/home-port4.png">
                     <div class="photo-icons">
                         <div class="row">
                             <div class="col-md-4">
@@ -59,14 +59,49 @@
                             <div class="col-md-4">
                                 <i class="fa fa-thumbs-down"></i>&nbsp;-1
                             </div>
-                            <div class="col-md-4 text-md-left">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;Position
+                            <div class="col-md-4">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;Map
                             </div>
                         </div>
                     </div>
                     <div class="photo-container-description">
                         <span class="photo-description">
-                            Configuration and logfile names: If the filenames you specify for many
+                            Configuration and logfile names: If the filenames you specify for many, Configuration and logfile names: If the filenames you specify for many, Configuration and logfile names: If the filenames you specify for many, Configuration and logfile names: If th.
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <!-- Photo End-->
+
+             <!-- Photo Start-->
+             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="photo-container">
+                    <div class="row photo-user">
+                        <div class="col-md-7">
+                            {{ $user->nickname }}
+                        </div>
+                        <div class="col-md-5 text-right">
+                            28/08/2019
+                        </div>
+                    </div>
+                    <div class="photo-border"></div>
+                    <img class="img-fluid" src="images/cocoon/portfolio/home-port4.png">
+                    <div class="photo-icons">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <i class="fa fa-thumbs-up"></i>&nbsp;+1
+                            </div>
+                            <div class="col-md-4">
+                                <i class="fa fa-thumbs-down"></i>&nbsp;-1
+                            </div>
+                            <div class="col-md-4">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;Map
+                            </div>
+                        </div>
+                    </div>
+                    <div class="photo-container-description">
+                        <span class="photo-description">
+                            Configuration and logfile names: If the filenames you specify for many, Configuration and logfil.
                         </span>
                     </div>
                 </div>
