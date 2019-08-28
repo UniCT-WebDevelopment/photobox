885fb719a051809fb96a2a85cbf580e05b969da0
Salvatore La Torre <latorresalvo@gmail.com>
2019-08-28 21:20:12 +0200

    add imgProfilo to user model

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'cognome', 'nickname', 'dataNascita', 
-        'email', 'password', 'bio'
+        'email', 'password', 'bio', 'imgProfilo'
     ];
 
     /**
