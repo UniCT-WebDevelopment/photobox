9a3c2980a3cc96cf7dbdcb81f9d1245a7c9ff7d5
Salvatore La Torre <latorresalvo@gmail.com>
2019-08-28 10:51:11 +0200

    clean code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <!-- ========== Meta Tags ========== -->
     <meta charset="UTF-8">
@@ -32,81 +33,81 @@
 <body>
     <div class="loader">
         <div class="loader-outter"></div>
-    <div class="loader-inner"></div>
-</div>
+        <div class="loader-inner"></div>
+    </div>
 
     <div class="body-container container-fluid">
-    <a class="menu-btn" href="javascript:void(0)">
-        <i class="ion ion-grid"></i>
-    </a>
-    <div class="row justify-content-center">
-        <!--=================== side menu ====================-->
-        <div class="col-lg-2 col-md-3 col-12 menu_block">
+        <a class="menu-btn" href="javascript:void(0)">
+            <i class="ion ion-grid"></i>
+        </a>
+        <div class="row justify-content-center">
+            <!--=================== side menu ====================-->
+            <div class="col-lg-2 col-md-3 col-12 menu_block">
 
-            <!-- profile photo -->
-            <div id="colorlib-aside" class="logo_box">
-                <h1 id="colorlib-logo">
-                    <span class="img" style="background-image: url(images/users/author.jpg);"></span>
-                    <a href="#">@yield('nome_utente')</a>
-                </h1>
-            </div>
-            <!-- profile photo end-->
+                <!-- profile photo -->
+                <div id="colorlib-aside" class="logo_box">
+                    <h1 id="colorlib-logo">
+                        <span class="img" style="background-image: url(images/users/author.jpg);"></span>
+                        <a href="#">@yield('nome_utente')</a>
+                    </h1>
+                </div>
+                <!-- profile photo end-->
 
-            <!--main menu -->
-            <div class="side_menu_section">
-                <ul class="menu_nav">
-                    <li class="active">
-                        <a href="feed">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Carica foto</a>
-                    </li>
-                    <li>
-                        <a href="#">Le mie foto</a>
-                    </li>
-                    <li>
-                        <a href="profile">Il mio Profilo</a>
-                    </li>
-                    <li>
-                        <a href="logout">Esci</a>
-                    </li>
-                </ul>
-            </div>
-            <!--main menu end -->
+                <!--main menu -->
+                <div class="side_menu_section">
+                    <ul class="menu_nav">
+                        <li class="active">
+                            <a href="feed">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Carica foto</a>
+                        </li>
+                        <li>
+                            <a href="#">Le mie foto</a>
+                        </li>
+                        <li>
+                            <a href="profile">Il mio Profilo</a>
+                        </li>
+                        <li>
+                            <a href="logout">Esci</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--main menu end -->
 
-            <!--filter menu -->
-            <div class="side_menu_section">
-                <h4 class="side_title">filtra per:</h4>
-                <ul  id="filtr-container"  class="filter_nav">
-                    <li  data-filter="*" class="active"><a href="javascript:void(0)" >tutte</a></li>
-                    <li data-filter=".branding"> <a href="javascript:void(0)">top of the week</a></li>
-                    <li data-filter=".design"><a href="javascript:void(0)">top of the mounth</a></li>
-                    <li data-filter=".photography"><a href="javascript:void(0)">the best of ever</a></li>
-                </ul>
-            </div>
-            <!--filter menu end -->
+                <!--filter menu -->
+                <div class="side_menu_section">
+                    <h4 class="side_title">filtra per:</h4>
+                    <ul id="filtr-container" class="filter_nav">
+                        <li data-filter="*" class="active"><a href="javascript:void(0)">tutte</a></li>
+                        <li data-filter=".branding"> <a href="javascript:void(0)">top of the week</a></li>
+                        <li data-filter=".design"><a href="javascript:void(0)">top of the mounth</a></li>
+                        <li data-filter=".photography"><a href="javascript:void(0)">the best of ever</a></li>
+                    </ul>
+                </div>
+                <!--filter menu end -->
 
-            <!--social and copyright -->
-            <div class="side_menu_bottom">
-                <div class="side_menu_bottom_inner">
-                    <div class="copy_right">
-                        <p>© 2019 {{ env('APP_NAME') }}. All rights reserved </p>
-                        <p>Design by: {{ env('AUTHORS') }}</p>
+                <!--social and copyright -->
+                <div class="side_menu_bottom">
+                    <div class="side_menu_bottom_inner">
+                        <div class="copy_right">
+                            <p>© 2019 {{ env('APP_NAME') }}. All rights reserved </p>
+                            <p>Design by: {{ env('AUTHORS') }}</p>
+                        </div>
                     </div>
                 </div>
+                <!--social and copyright end -->
+
             </div>
-            <!--social and copyright end -->
+            <!--=================== side menu end====================-->
 
+            <!--=================== content body ====================-->
+            <div class="col-lg-10 col-md-9 col-12 body_block  align-content-center">
+                @yield('content')
+            </div>
+            <!--=================== content body end ====================-->
         </div>
-        <!--=================== side menu end====================-->
-
-        <!--=================== content body ====================-->
-        <div class="col-lg-10 col-md-9 col-12 body_block  align-content-center">
-            @yield('content')
-        </div>
-        <!--=================== content body end ====================-->
     </div>
-</div>
 
     <!-- jquery -->
     <script src="js/cocoon/jquery.min.js"></script>
@@ -135,4 +136,5 @@
     <!-- Custom js -->
     <script src="js/cocoon/main.js"></script>
 </body>
+
 </html>
