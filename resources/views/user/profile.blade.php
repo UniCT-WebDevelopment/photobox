41bc9694f91036880d4dfb77656c8bce29b3ce34
Salvatore La Torre <latorresalvo@gmail.com>
2019-11-03 12:56:10 +0100

    cambiata label email

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -11,7 +11,7 @@
     <h6><strong>Nome</strong>: {{ $user->nome }}</h6>
     <h6><strong>Cognome</strong>: {{ $user->cognome }}</h6>
     <h6><strong>Username</strong>: {{ $user->nickname }}</h6>
-    <h6><strong>eMail</strong>: {{ $user->email }}</h6>
+    <h6><strong>Email</strong>: {{ $user->email }}</h6>
     <h6><strong>Data di Nascita</strong>: {{ date('d-m-Y', strtotime($user->dataNascita)) }}</h6>
     <h6><strong>Biografia</strong>: {{ $user->bio }}</h6>
    
