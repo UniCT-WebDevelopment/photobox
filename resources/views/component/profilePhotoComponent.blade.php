b8a59b8b0abfc957ff818cfd22bb0f870e26b63a
Salvatore La Torre <latorresalvo@gmail.com>
2020-01-02 17:43:51 +0100

    vai al profilo click su foto

--- resources/views/component/profilePhotoComponent.blade.php
+++ resources/views/component/profilePhotoComponent.blade.php
@@ -1,11 +1,13 @@
 <div id="colorlib-aside" class="logo_box">
-    <h1 id="colorlib-logo">
-        {{-- @yield('photoProfile') --}}
-        @if(!empty($user->imgProfilo))
-            <span class="img" style="background-image: url(storage/users/profile/{{$user->id}}/{{$user->imgProfilo}});"></span>
-        @else 
+    <a href="/profile">
+        <h1 id="colorlib-logo">
+            @if(!empty($user->imgProfilo))
+            <span class="img"
+                style="background-image: url(storage/users/profile/{{$user->id}}/{{$user->imgProfilo}});"></span>
+            @else
             <span class="img" style="background-image: url(images/users/default.png);"></span>
-        @endif
-        <a href="/profile">{{$user->nome}} {{$user->cognome}}</a>
-    </h1>
+            @endif
+            {{$user->nome}} {{$user->cognome}}
+        </h1>
+    </a>
 </div>
